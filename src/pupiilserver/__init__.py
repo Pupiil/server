4a5c26a02c6d0e05e5077e371da13cf64d858a30
Saif Ul Islam <saifulislam84210@gmail.com>
2022-05-06 11:57:17 +0500

    :pushpin: fix: Setting up packages, can't make setup.py, setup.cfg work properly
    Signed-off-by: Saif Ul Islam <saifulislam84210@gmail.com>

---
+++ src/pupiilserver/__init__.py
@@ -0,0 +1,16 @@
+"""
+pupiil_server.__init__
+~~~~~~~~~~~~~~~~~~
+
+- License: MIT LICENSE
+"""
+
+from . import server
+"""
+pupiil_server.__init__
+~~~~~~~~~~~~~~~~~~
+
+- License: MIT LICENSE
+"""
+
+from . import server
